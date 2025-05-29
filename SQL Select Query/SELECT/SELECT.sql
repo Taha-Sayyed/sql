75e50d948f1b6803fbcccb377d9877c5e9f500e6
Taha-Sayyed <codetaha786@gmail.com>
2025-05-29 16:04:21 +0530

    SELECT Queries

---
+++ SQL Select Query/SELECT/SELECT.sql
@@ -0,0 +1,26 @@
+-- Retrieve all the customers
+select * from customers
+
+/*Output
+id	first_name	country	score
+1	Maria		Germany	350
+2	John		USA	    900
+3	Georg		UK	    750
+4	Martin		Germany	500
+5	Peter		USA	    0
+*/
+
+
+-- Retrieve all the orders
+
+select * from orders
+
+/*
+
+order_id	customer_id		order_id	sales
+1001		1				2021-01-11	35
+1002		2				2021-04-05	15
+1003		3				2021-06-18	20
+1004		6				2021-08-31	10
+
+*/
