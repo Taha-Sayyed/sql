b36eaed16e0c532987db7df930da0cf515bf37eb
Taha-Sayyed <codetaha786@gmail.com>
2025-05-29 16:22:02 +0530

    Query conaining WHERE Clause

---
+++ SQL Select Query/WHERE/query_1.sql
@@ -0,0 +1,16 @@
+-- Retreive the customers whose score is not equal to zero
+
+use MyDatabase
+
+select * 
+from customers
+where score !=0
+
+
+-- Retreive the customers from Germany 
+
+select *
+from customers
+where country='Germany'
+
+
