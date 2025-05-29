e44aa7d8babc0ffa15cb41b9121866ca5fbafc63
Taha-Sayyed <codetaha786@gmail.com>
2025-05-29 16:10:04 +0530

    SELECT Column

---
+++ SQL Select Query/SELECT/SELECT_COLUMN.sql
@@ -0,0 +1,18 @@
+-- Retrieve each customers's name,country,score
+
+SELECT 
+	first_name,
+	country,
+	score
+from customers
+
+/*
+First_name	country		score
+Maria		Germany		350
+John		USA			900
+Georg		UK			750
+Martin		Germany		500
+Peter		USA			0
+
+
+*/
