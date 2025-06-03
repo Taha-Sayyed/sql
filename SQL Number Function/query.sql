a92e11d7ad58354a0d0e79362be7c238ab9d59ca
Taha-Sayyed <codetaha786@gmail.com>
2025-06-03 20:18:10 +0530

    Number function

---
+++ SQL Number Function/query.sql
@@ -0,0 +1,16 @@
+-- 1] Round
+
+select 
+	3.516 as Value,
+	round(3.516,2) as round_2,
+	round(3.516,1) as round_1,
+	round(3.516,0) as round_0;
+
+-- 2] abs: return the absolute positive value of a number, removing any negatve sign
+
+select
+	-10,
+	abs(-10);
+
+
+
